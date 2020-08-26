8251abe81b1b53fb1d901d677c85a6c58472cc69
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-26 04:01:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,9 +17,7 @@ end
 
 
 def assign_rooms(attendees)
-  room_assignments = []
-  attendees.each_with_index do |name, index|
-    room_assignments.push("Hello, #{badge_maker(name)} You'll be assigned to room #{index +1}!")
+  
   end
   # room_assignments = []
   # attendees.each_with_index do|name, index|
